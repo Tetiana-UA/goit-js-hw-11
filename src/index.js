8da96fb94e3e38640dafff585217ff8b7bf47a0a
Tetiana-UA <izidaxm85@gmail.com>
2023-11-28 02:04:09 +0100

    4

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { axios } from "axios";
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import  axios  from "axios";
+import  {Notify}  from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from "simplelightbox";
 // Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
@@ -10,24 +10,35 @@ const refs ={
     button: document.querySelector(".search-btn"),
     galleryResults: document.querySelector(".gallery"),
 }
-let page; 
+let page = 1; 
 
-refs.form.addEventListener("submit", handleSubmit)
+refs.form.addEventListener("submit", handleSubmit);
 
-async function handleSubmit(event) {
+
+function handleSubmit(event) {
     event.preventDefault();
-    const {searchQuery}=event.currentTarget.elements
+    const {searchQuery} = refs.form.elements; 
     
-    getItems(searchQuery)
-.then((data)=>{
-    refs.galleryResults.innerHTML=createMarkup(data);
+        
+    //Викликаємо функцію запиту на сервер, передаємо їй аргументом значення інпуту,  та обробляємо результат виклику data. Рендеримо розмітку карток зображень.
+    getItems(searchQuery.value)
+    .then((data)=>{
+    refs.galleryResults.innerHTML=createMarkup(data.hits);
+    Notify.success(`"Hooray! We found ${data.totalHits} images."`);
+
+    //Перевірка -якщо бекенд повертає порожній масив, то виводимо повідомлення
+    if (data === null) {
+        Notify.failure('sorry,there are no images matching your search query. Please try again.');
+    }
 })
-.catch((err)=>console.log(err));
+.catch((err) => console.log(err));
+//.finally(() => refs.form.reset());
     
 }
 
+//Оголошення функції запиту на сервер (використовуємо async/await та axios)
 async function getItems(searchQuery) {
-    const BASE_URL="https://pixabay.com/api";
+    const BASE_URL="https://pixabay.com/api/";
     const APY_KEY="40927471-d9019deac4f1ad7f4676e914d";
 
     const params=new URLSearchParams({
@@ -38,58 +49,41 @@ async function getItems(searchQuery) {
         q: searchQuery,
     })
 
-     //1 варіант (запит на сервер через axios) => axios.axios is undefined???????????
     const {data} = await axios.get(`${BASE_URL}?${params}`)
-    
     return data;
-
-
-    //2 варіант (запит на сервер через fetch) => в консолі помилка??????????
-    //const resp = await fetch(`${BASE_URL}?${params}`);
-      //  if(!resp.ok) {
-      //      throw new Error("404 not found")
-      //  }
-      //  const data = await response.json();
-      //  return data;
     }
     
 
-    //Перевірка функції запиту на сервер 
-    getItems("flower")
-    .then((data)=>console.log(data))
-    .catch((err)=>console.log(err));
-
-
-
-
+//Функція створення розмітки картки зображення
     function createMarkup(arr) {
         return arr
-        .map(({largeImageURL, webformatURL, tags, likes, views, comments, downloads}) => `
+        .map((hit) => `
     <div class="photo-card">
-    <a class="photo__link" href="${largeImageURL}">
-    <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+    <a class="photo__link" href="${hit.largeImageURL}">
+    <img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" />
     </a>
     <div class="info">
     <p class="info-item">
-        <b>Likes ${likes}</b>
+        <b>Likes ${hit.likes}</b>
     </p>
     <p class="info-item">
-        <b>Views ${views}</b>
+        <b>Views ${hit.views}</b>
     </p>
     <p class="info-item">
-        <b>Comments ${comments}</b>
+        <b>Comments ${hit.comments}</b>
     </p>
     <p class="info-item">
-        <b>Downloads ${downloads}</b>
+        <b>Downloads ${hit.downloads}</b>
     </p>
     </div>
     
     </div>`) 
     }
 
-
- 
-
-
-
-
+    //Модальне вікно
+    const lightbox = new SimpleLightbox(".gallery a", {
+        captionsData: "alt",
+        captionPosition: "bottom",
+        captionDelay: 250,
+        close: "true",
+    })
